31c3694fd11fb923100c8a9e3360e6faa3421f1b
ritacatuxo <ritafdezortiz@gmail.com>
2024-04-13 10:49:50 +0200

    SDI-2324-804-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi2324-804-lab-soap-server/sdi2324-804-lab-soap-server/src/main/java/com/uniovi/sdi/Sdi2324804LabSoapServerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324804LabSoapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324804LabSoapServerApplication.class, args);
+    }
+
+}
---
+++ sdi2324-804-lab-soap-server/sdi2324-804-lab-soap-server/src/test/java/com/uniovi/sdi/Sdi2324804LabSoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2324804LabSoapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
