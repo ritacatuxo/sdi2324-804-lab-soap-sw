86d48f7522d27f33b7ea4afd9b2237f7d5b53c9e
ritacatuxo <ritafdezortiz@gmail.com>
2024-04-15 11:48:18 +0200

    SDI-2324-804-SW-11.1- Servicio SOAP Cliente.

---
+++ sdi2324-804-lab-soap-sw-client/sdi2324-804-lab-soap-sw-client/src/main/java/com/uniovi/sdi/Sdi2324804LabSoapSwClientApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324804LabSoapSwClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324804LabSoapSwClientApplication.class, args);
+    }
+
+}
---
+++ sdi2324-804-lab-soap-sw-client/sdi2324-804-lab-soap-sw-client/src/main/java/com/uniovi/sdi/wsdl/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.2 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2024.04.15 a las 11:37:26 AM CEST 
+//
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://uniovi.com/sdi/soap/ws", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uniovi.sdi.wsdl;
